cc6a49abd0514fbc2c3f550ffe6c923c8cf38ce5
wcampbell <wcampbell1995@gmail.com>
2021-06-02 22:03:05 -0400

    Add readme example as test
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- tests/test.rs
+++ tests/test.rs
@@ -50,3 +50,39 @@ fn use_bits_from_type() {
 \end{figure}"#;
     assert_eq!(Testing::latex_output(), expected);
 }
+
+#[test]
+fn readme() {
+    use bintex::{BinTex, BinTexOutput};
+
+    #[allow(dead_code)]
+    #[derive(BinTex)]
+    #[bintex(bit_width = 32)]
+    struct Ipv6 {
+        #[deku(bits = "4")]
+        version: u8,
+        #[deku(bits = "6")]
+        ds: u8,
+        #[deku(bits = "2")]
+        ecn: u8,
+        #[deku(bits = "20")]
+        label: u32,
+        length: u16,
+        next_header: u8,
+        hop_limit: u8,
+        src: u32,
+        dst: u32,
+    }
+
+    let expected = r#"\begin{figure}
+\begin{bytefield}{32}
+\bitheader{0-31} \\
+\bitbox{4}{version} & \bitbox{6}{ds} & \bitbox{2}{ecn} & \bitbox{20}{label} \\
+\bitbox{16}{length} & \bitbox{8}{next\_header} & \bitbox{8}{hop\_limit} \\
+\bitbox{32}{src} \\
+\bitbox{32}{dst} \\
+\end{bytefield}
+\caption{Ipv6}
+\end{figure}"#;
+    assert_eq!(Ipv6::latex_output(), expected);
+}
